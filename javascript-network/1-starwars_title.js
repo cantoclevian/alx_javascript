6dff6cb7dfed6c4ca5aa8bc4ac292e4efac5c1ae
cantonweke <cantonotieno@gmail.com>
2023-09-10 22:02:16 -0700

    2nd commit

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,28 @@
+# a script that prints the title of a Star Wars movie where the episode number matches a given integer 
+
+
+const request = require('request');
+
+// Function to fetch the movie title by episode number
+function getMovieTitleByEpisode(episodeNumber) {
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${episodeNumber}`;
+
+  request(apiUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      console.log(`Episode ${movieData.episode_id}: ${movieData.title}`);
+    } else {
+      console.error(`Error fetching movie data: ${error}`);
+    }
+  });
+}
+
+// Usage example: Pass the episode number as a command line argument
+const episodeNumber = process.argv[2];
+
+if (!episodeNumber) {
+  console.error('Please provide an episode number as a command line argument.');
+} else {
+  getMovieTitleByEpisode(episodeNumber);
+}
+
